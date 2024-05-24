90a29f0c1f8ef8bab35ee4637c32a1dd1b38d1b0
ganesh12112002 <145575031+ganesh12112002@users.noreply.github.com>
2024-05-24 00:20:17 -0700

    Add files via upload
    last push js folder.all file inside the js folder

---
+++ js/datatype_example.js
@@ -0,0 +1,30 @@
+// number
+// let userid=21820
+// console.log(userid);
+
+// Boolean
+// let stname=true
+// console.log("iseligible for vote",true);
+
+// string
+
+// let fname="ganesh";
+// let age=23;
+// console.log("hello",fname,"your age is ",age);
+
+// Object
+
+// array
+
+// let llf=["rajan",'santh',"sunil","prasad","mukesh","praveen","sathish"];
+// console.log(llf[2],llf[3]);
+ 
+// js object
+// let person={
+//     fname:"ganesh",
+//     age:21,
+//     gender:"male"
+// }
+// console.log(person['gender']);
+
+
